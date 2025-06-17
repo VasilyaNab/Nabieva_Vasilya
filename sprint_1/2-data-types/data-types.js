3ebdfa6bc9d63ef98e9fc3b9778c32b2c173991c
Vasilya <lara_nabieva@bk.ru>
2025-06-17 15:48:27 +0400

    task 2

--- sprint_1/2-data-types/data-types.js
+++ sprint_1/2-data-types/data-types.js
@@ -8,6 +8,29 @@
 
 
 // Код задания 1
+let a = "vasilya";
+let b = 18;
+let c = true;
+let d = null;
+let e;
+let f = Symbol("f");
+let g = BigInt("1111111111111111111111111")
+let product = {
+    name: "Apple AirPods Max",
+    color: "purple",
+    price: 60990,
+};
+let categories = ["MacBook", "AirPods", "iPad"];
+
+console.log(typeof a) // string
+console.log(typeof b) // number
+console.log(typeof c) // boolean
+console.log(typeof d) // object
+console.log(typeof e) // undefined
+console.log(typeof f) // symbol
+console.log(typeof g) // bigint
+console.log(typeof product) // object
+console.log(typeof categories) // object
 
 
 /**
@@ -18,6 +41,12 @@
 
 
 // Код задания 2
+let numbers = "123";
+// Варианты изменения строки в число
+let changeNumType = Number(numbers);
+let changeNumType2 = +"123";
+let example1 = numbers + 5; // 1235. число, которое мы прибавляем, преобразовывается в строку
+let example2 = changeNumType + 5; //128. выполняется арифметическая опреация, происходит сложение чисел
 
 
 /**
@@ -28,6 +57,25 @@
 
 
 // Код задания 3
+let info = 10;
+function check(info) {
+    if (typeof info == "string") {
+        console.log("Это строка");
+    }else if (typeof info == "number"){
+        console.log("Это число");
+    }else if (typeof info == "boolean"){
+        console.log("Это логический тип");
+    }else if (typeof info == "undefined"){
+        console.log("Это переменная без значения");
+    }else if (typeof info == "symbol"){
+        console.log("Это символ");
+    }else if (typeof info == "bigint"){
+        console.log("Это переменная с большим количеством целых чисел");
+    }else{
+        console.log("Это объект");
+    }
+}
+check(info);
 
 
 /**
@@ -44,3 +92,8 @@
  **/
 
 // Код задания 4
+// Разница между undefined и null в том, что undefined означает, что значение еще не задано, а null значение намеренно пустое. Да и по typeof они выдают разные типы
+console.log(a == b);  // true
+console.log(a === b); // false
+console.log(typeof a); // undefined
+console.log(typeof b); // object
